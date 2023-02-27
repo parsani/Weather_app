cef245b26b03b2639131839bda9c0323c6f00fa5
parsa niroomand <parsaniroomand@gmail.com>
2023-02-27 15:09:22 +0330

    Some Change

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -11,15 +11,15 @@ class Forecast {
     const weather = await this.getWeather(cityDets.Key);
     return { cityDets, weather };
   }
-  async getCity(city) {
-    const query = `?apikey=${this.key}&q=${city}`;
-    const response = await fetch(this.cityURI + query);
-    const data = await response.json();
-    return data[0];
-  }
   async getWeather(id) {
     const query = `${id}?apikey=${this.key}`;
     const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+  async getCity(city) {
+    const query = `?apikey=${this.key}&q=${city}`;
+    const response = await fetch(this.cityURI + query);
     const data = await response.json();
     return data[0];
   }
